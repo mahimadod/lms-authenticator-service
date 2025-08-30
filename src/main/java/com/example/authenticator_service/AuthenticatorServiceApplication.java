a3375f44d84ee8914f7ac194ba9c063f4862e3ac
mahima <dodmahima@gmail.com>
2025-08-30 16:55:21 +0530

    initial commit

--- src/main/java/com/example/authenticator_service/AuthenticatorServiceApplication.java
+++ src/main/java/com/example/authenticator_service/AuthenticatorServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.authenticator_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthenticatorServiceApplication {
 
 	public static void main(String[] args) {
