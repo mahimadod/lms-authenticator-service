89a3786e57d62f990ea92da70cac410e9934a19d
mahima <dodmahima@gmail.com>
2025-09-10 17:10:06 +0530

    code for kibana config

--- src/main/java/com/example/authenticator_service/controller/AuthController.java
+++ src/main/java/com/example/authenticator_service/controller/AuthController.java
@@ -5,11 +5,13 @@ import com.example.authenticator_service.service.AuthenticationService;
 import com.example.authenticator_service.service.AuthenticationServiceImpl;
 import com.example.authenticator_service.util.JwtUtil;
 import com.example.authenticator_service.dtos.AuthRequest;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -26,12 +28,14 @@ public class AuthController {
 
     @PostMapping(value="/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        MDC.put("service", "Auth-service");
         System.out.println("CALLING AGAIN");
         return authenticationService.authenticateLogin(authRequest);
     }
 
     @GetMapping(value="/validate")
     public ResponseEntity<AuthResponse> validate(@RequestParam String token) {
+
         log.info("Validating token: "+token);
         return authenticationService.validateToken(token);
     }
